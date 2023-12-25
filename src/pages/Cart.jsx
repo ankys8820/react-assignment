79284ed09e8887bc07932817d6e8ce3225137234
ankys8820 <140976749+ankys8820@users.noreply.github.com>
2023-12-25 22:52:48 +0530

    Update Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import CartItem from "../components/CartItem";
+import { useSelector } from "react-redux";
 
 const Cart = () => {
+  const products = useSelector((state) => state.bazar.productData);
+  const getTotal = () => {
+    let total = 0;
+    products.forEach((item) => (total = total + item.price * item.quantity));
+    return total;
+  };
   return (
     <div className="max-w-screen-xl mx-auto py-20 flex">
       <CartItem />
@@ -9,7 +16,7 @@ const Cart = () => {
         <div className="flex flex-col gap-6 border-b-[1px] border-b-gray-400 pb-6">
           <h2 className="text-2xl font-medium">Cart Totals</h2>
           <p className="flex items-center gap-4 text-base">
-            Subtota <span className="font-bold text-lg">${200}</span>
+            Subtota <span className="font-bold text-lg">${getTotal()}</span>
           </p>
           <p className="flex items-start gap-4 text-base">
             Shipping{" "}
@@ -20,7 +27,7 @@ const Cart = () => {
           </p>
         </div>
         <p className="flex justify-between font-semibold mt-6">
-          Total <span className="text-xl font-bold">${200}</span>
+          Total <span className="text-xl font-bold">${getTotal()}</span>
         </p>
         <button className="text-base bg-cyan-500 text-white w-full py-3 mt-6 hover:bg-cyan-800 duration-300">
           Proceed to Checkout
